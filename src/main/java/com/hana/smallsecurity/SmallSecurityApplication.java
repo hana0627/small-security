6c04c2449fc3787c42024cc31638b053080e66bc
hana6270 <105623102+hana6270@users.noreply.github.com>
2023-04-27 19:58:21 +0900

    Initial commit

---
+++ src/main/java/com/hana/smallsecurity/SmallSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.hana.smallsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hana/smallsecurity/SmallSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.smallsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
